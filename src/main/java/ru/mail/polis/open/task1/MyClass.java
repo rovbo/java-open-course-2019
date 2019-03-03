d3e0530cd2388a505b20ebb88f87060f07f61f0b
rovbo <47986765+rovbo@users.noreply.github.com>
2019-03-03 21:02:53 +0300

    homework1

---
+++ src/main/java/ru/mail/polis/open/task1/MyClass.java
@@ -0,0 +1,17 @@
+package ru.mail.polis.open.task1;
+
+    public class MyClass implements FizzBuzz {
+        public void print(int from, int to) {
+            for (int i = from; i <= to; i++) {
+                if ((i % 15) == 0)
+                    System.out.println("FizzBuzz");
+                else if ((i % 5) == 0)
+                    System.out.println("Buzz");
+                else if ((i % 3) == 0)
+                    System.out.println("Fizz");
+                else
+                    System.out.println(i);
+            }
+
+        }
+    }
